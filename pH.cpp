c1095fef8ba4bfa340134ad0ad191fd3e8bbcb08
pfox27 <80210760+pfox27@users.noreply.github.com>
2024-01-25 13:53:32 -0500

    Add files via upload

---
+++ pH.cpp
@@ -0,0 +1,41 @@
+/* A program to calculate the pH of an acid or base, given the concentration of H+ ions or OH- ions. */
+
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+
+int main(){
+
+	char solution;
+	float concentration = 0;
+	float pH = 0;
+	float pOH = 0;
+
+	cout << ("\nIf the solution is an acid enter A, if a base enter B: \n");
+	cin >> solution;
+	if(solution == 'A'){
+		cout << ("\nPlease enter the concentration of H+ ions in the acid in moles/litre, using scientific notation (example: 3.5E-6): \n");
+		cin >> concentration;
+		pH = -10 * log(concentration);
+		cout << ("/n the pH of the acid is ", pH);
+	}
+
+	if(solution == 'B'){
+		cout << ("\nPlease enter the concentration of OH- ions in the base in moles/litre, using scientific notation (example: 3.5E-6): \n");
+		cin >> concentration;
+		pOH = -10 * log(concentration);
+		pH = 14 - pOH;
+		cout << ("/n the pH of the base is ", pH);
+	}
+return 0;
+}
+
+
+
+
+
+
+
+
+
